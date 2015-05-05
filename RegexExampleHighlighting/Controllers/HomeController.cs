78534b18789a2b827672ed334a036aafbba4b459
apetruccione <lagnath@gmail.com>
2015-05-05 12:29:28 -0500

    Created example of Regex highlighting

--- RegexExampleHighlighting/Controllers/HomeController.cs
+++ RegexExampleHighlighting/Controllers/HomeController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Text.RegularExpressions;
 
 namespace RegexExampleHighlighting.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
+            //the term you want to search for, and the color you want to highlight.
+            ViewData["searchterm"] = "aster";
+            ViewData["highlightcolor"] = "yellow";
+
+            // Get a string and send it over to the view, which will call the helper method in the RegexHelp.cs file.
+            ViewData["temp"] = "aster faster plaster mastering forecaster.";
             return View();
         }
 
---
+++ RegexExampleHighlighting/RegexHelp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text.RegularExpressions;
+
+namespace RegexExampleHighlighting
+{
+    public static class RegexHelp
+    {
+
+
+        // Regex helper method, take from this blog:
+        //http://www.mikesdotnetting.com/article/139/highlighting-keywords-found-in-search-results
+        /// <summary>
+        /// Wraps matched strings in HTML span elements styled with a background-color
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="keywords">Comma-separated list of strings to be highlighted</param>
+        /// <param name="cssClass">The Css color to apply</param>
+        /// <param name="fullMatch">false for returning all matches, true for whole word matches only</param>
+        /// <returns>string</returns>
+        public static string HighlightKeyWords(this string text, string keywords, string cssClass, bool fullMatch)
+        {
+            if (text == String.Empty || keywords == String.Empty || cssClass == String.Empty)
+                return text;
+            var words = keywords.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            if (!fullMatch)
+                return words.Select(word => word.Trim()).Aggregate(text,
+                             (current, pattern) =>
+                             Regex.Replace(current,
+                                             pattern,
+                                               string.Format("<span style=\"background-color:{0}\">{1}</span>",
+                                               cssClass,
+                                               "$0"),
+                                               RegexOptions.IgnoreCase));
+            return words.Select(word => "\\b" + word.Trim() + "\\b")
+                        .Aggregate(text, (current, pattern) =>
+                                  Regex.Replace(current,
+                                  pattern,
+                                    string.Format("<span style=\"background-color:{0}\">{1}</span>",
+                                    cssClass,
+                                    "$0"),
+                                    RegexOptions.IgnoreCase));
+
+        }
+    }
+}
